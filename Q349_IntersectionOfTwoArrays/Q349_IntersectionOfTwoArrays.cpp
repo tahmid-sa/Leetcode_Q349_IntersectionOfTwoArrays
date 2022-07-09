39e57e23a56ea1a73a144b3b2da716a2024b0c97
Tahmid Sajin <tahmid.sajin.1999@gmail.com>
2022-07-09 02:13:52 -0400

    Adding files to github

---
+++ Q349_IntersectionOfTwoArrays/Q349_IntersectionOfTwoArrays.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> inter;
+    int currNum = -1;
+
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+
+    for (int i = 0; i < nums1.size(); i++) {
+        if (nums1[i] == currNum) {
+            continue;
+        }
+
+        for (int j = 0; j < nums2.size(); j++) {
+            if (nums1[i] == nums2[j]) {
+                if (nums2[j] == currNum) {
+                    continue;
+                }
+                else {
+                    inter.push_back(nums2[j]);
+                    currNum = nums2[j];
+                }
+            }
+        }
+    }
+
+    return inter;
+}
+
+int main()
+{
+    vector<int> ivec = { 1, 2, 3, 4, 5 };
+    vector<int> ivec2 = { 2, 4, 5 };
+
+    intersection(ivec, ivec2);
+
+    return 0;
+}
